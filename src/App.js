d5e1059115a27e60070041ff6ecdad2acd834164
Fede <ferrelli.mazza@gmail.com>
2022-03-04 17:43:26 -0300

    Create project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div >
+      <h1>
+        Nuevo Proyecto!!!
+        New Project!!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
